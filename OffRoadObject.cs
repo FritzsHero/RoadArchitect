932b511af0bb7f79e778b462e45314fbbb1b358e
FritzsHero <fritzshero@gmail.com>
2020-02-21 14:46:11 +0100

    Changed OffRoadObject namespace to RoadArchitect

--- OffRoadObject.cs
+++ OffRoadObject.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 #endregion
 
 
-public class OffRoadObject : MonoBehaviour
+namespace RoadArchitect
 {
-    /// <summary> The color of the offRoadNode Gizmo inside Editor </summary>
-    public static readonly Color offRoadNodeColor = new Color(1f, 0f, 0.5f, 0.75f);
-    /// <summary> The color of the selected offRoadNode Gizmo inside Editor </summary>
-    public static readonly Color offRoadSelectedNodeColor = new Color(1f, 0f, 0.8f, 1f);
+    public class OffRoadObject : MonoBehaviour
+    {
+        /// <summary> The color of the offRoadNode Gizmo inside Editor </summary>
+        public static readonly Color offRoadNodeColor = new Color(1f, 0f, 0.5f, 0.75f);
+        /// <summary> The color of the selected offRoadNode Gizmo inside Editor </summary>
+        public static readonly Color offRoadSelectedNodeColor = new Color(1f, 0f, 0.8f, 1f);
+    }
 }
