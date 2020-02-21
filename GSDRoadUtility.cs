85242aee7a47d38326dd9a5801ea7f872261245e
FritzsHero <fritzshero@gmail.com>
2020-02-21 14:55:35 +0100

    Moved Navigation into separated file

--- GSDRoadUtility.cs
+++ GSDRoadUtility.cs
@@ -8198,58 +8198,5 @@ namespace RoadArchitect
         #endregion
         #endregion
     }
-
-
-    public static class GSDNavigation
-    {
-        public static void UpdateConnectedNodes()
-        {
-            InitResetNavigationData();
-
-            Object[] allSplines = GameObject.FindObjectsOfType(typeof(SplineC));
-
-            //Store connected spline nodes on each other:
-            SplineN node;
-            foreach (SplineC spline in allSplines)
-            {
-                int nodeCount = spline.nodes.Count;
-                for (int i = 0; i < nodeCount; i++)
-                {
-                    node = spline.nodes[i];
-                    //Add next node if not last node:
-                    if ((i + 1) < nodeCount)
-                    {
-                        node.connectedID.Add(spline.nodes[i + 1].id);
-                        node.connectedNode.Add(spline.nodes[i + 1]);
-                    }
-                    //Add prev node if not first node:
-                    if (i > 0)
-                    {
-                        node.connectedID.Add(spline.nodes[i - 1].id);
-                        node.connectedNode.Add(spline.nodes[i - 1]);
-                    }
-                }
-            }
-        }
-
-
-        public static void InitResetNavigationData()
-        {
-            Object[] allSplines = GameObject.FindObjectsOfType(typeof(SplineC));
-            int splineCount = 0;
-            int nodeCount = 0;
-            foreach (SplineC spline in allSplines)
-            {
-                splineCount += 1;
-                foreach (SplineN node in spline.nodes)
-                {
-                    nodeCount += 1;
-                    node.ResetNavigationData();
-                }
-                spline.ResetNavigationData();
-            }
-        }
-    }
-
 #endif
 }
