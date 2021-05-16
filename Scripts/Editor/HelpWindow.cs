da5de90434e05419be919dcdeee58e4bf40f614a
FritzsHero <fritzshero@gmail.com>
2021-05-16 16:54:58 +0200

    Updated HelpWindow content and links

--- Scripts/Editor/HelpWindow.cs
+++ Scripts/Editor/HelpWindow.cs
@@ -22,9 +22,9 @@ namespace RoadArchitect
 
             if (GUILayout.Button("Click here to open online manual", EditorStyles.toolbarButton, GUILayout.Width(310f)))
             {
-                Application.OpenURL("https://github.com/MicroGSD/RoadArchitect/wiki");
+                Application.OpenURL("https://github.com/FritzsHero/RoadArchitect/wiki");
             }
-            EditorGUILayout.LabelField("https://github.com/MicroGSD/RoadArchitect/wiki");
+            EditorGUILayout.LabelField("https://github.com/FritzsHero/RoadArchitect/wiki");
 
             if (GUILayout.Button("Click here to open offline manual", EditorStyles.toolbarButton, GUILayout.Width(310f)))
             {
@@ -36,69 +36,31 @@ namespace RoadArchitect
 
 
             EditorGUILayout.LabelField("Please visit us or reach out to us on Github (links below) with any questions or comments.");
-            EditorGUILayout.LabelField("You can also check out the following Sites, for an Beta of RoadArchitect:");
-            EditorGUILayout.LabelField("If you encounter Bugs or have a Feature Suggestion, you can submit them on one of the following sites:");
+            EditorGUILayout.LabelField("If you encounter Bugs or have a Feature Suggestion, you can submit them on the following sites:");
 
 
             GUILayout.Space(12f);
 
-
-            EditorGUILayout.LabelField("RoadArchitect 1.7 and above", EditorStyles.boldLabel);
-
-            GUILayout.Space(4f);
-
-            if (GUILayout.Button("FritzsHero's RoadArchitect Repository", EditorStyles.toolbarButton, GUILayout.Width(310f)))
+            if (GUILayout.Button("RoadArchitect Repository", EditorStyles.toolbarButton, GUILayout.Width(310f)))
             {
-                Application.OpenURL("https://github.com/FritzsHero/RoadArchitect/tree/master");
+                Application.OpenURL("https://github.com/FritzsHero/RoadArchitect");
             }
-            EditorGUILayout.LabelField("https://github.com/FritzsHero/RoadArchitect/tree/master");
+            EditorGUILayout.LabelField("https://github.com/FritzsHero/RoadArchitect");
 
             GUILayout.Space(4f);
 
-            if (GUILayout.Button("FritzsHero's RoadArchitect Issues", EditorStyles.toolbarButton, GUILayout.Width(310f)))
+            if (GUILayout.Button("RoadArchitect Issues", EditorStyles.toolbarButton, GUILayout.Width(310f)))
             {
                 Application.OpenURL("https://github.com/FritzsHero/RoadArchitect/issues");
             }
             EditorGUILayout.LabelField("https://github.com/FritzsHero/RoadArchitect/issues");
-
-
             GUILayout.Space(12f);
-
-
-            EditorGUILayout.LabelField("RoadArchitect 1.6 and older", EditorStyles.boldLabel);
-
-            GUILayout.Space(4f);
-
-            EditorGUILayout.BeginVertical();
-
-            if (GUILayout.Button("MicroGSD's RoadArchitect Repository", EditorStyles.toolbarButton, GUILayout.Width(310f)))
-            {
-                Application.OpenURL("https://github.com/MicroGSD/RoadArchitect/tree/master");
-            }
-            EditorGUILayout.LabelField("https://github.com/MicroGSD/RoadArchitect/tree/master");
-
-            EditorGUILayout.EndVertical();
-
-            GUILayout.Space(4f);
-
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.BeginVertical();
-
-            if (GUILayout.Button("MicroGSD's RoadArchitect Issues", EditorStyles.toolbarButton, GUILayout.Width(310f)))
-            {
-                Application.OpenURL("https://github.com/MicroGSD/RoadArchitect/issues");
-            }
-            EditorGUILayout.LabelField("https://github.com/MicroGSD/RoadArchitect/issues");
-
-            EditorGUILayout.EndVertical();
-
-            EditorGUILayout.EndHorizontal();
         }
 
 
         public void Initialize()
         {
-            Rect rect = new Rect(340, 170, 500, 500);
+            Rect rect = new Rect(340, 170, 400, 400);
             position = rect;
             Show();
             titleContent.text = "Help Info";
