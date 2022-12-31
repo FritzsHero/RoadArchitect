af5f5fa0e0ca8842c2b783c95c9adca1f07a563e
Sky4Flowers <24479340+Sky4Flowers@users.noreply.github.com>
2022-12-31 17:15:48 +0100

    Fixed ThreeWay Intersections (#47)
    - Corrected sign/trafic light placing
    - Fixed shoulder bug
    
    Case:
    End of spline is connected with a middle node of own spline

--- Scripts/RoadIntersection.cs
+++ Scripts/RoadIntersection.cs
@@ -292,24 +292,25 @@ namespace RoadArchitect
                 isSameSpline = true;
             }
 
-            if (isSameSpline)
-            {
-                if (_node1.idOnSpline < _node2.idOnSpline)
-                {
-                    node1 = _node1;
-                    node2 = _node2;
-                }
-                else
-                {
-                    node1 = _node2;
-                    node2 = _node1;
-                }
-            }
-            else
-            {
-                node1 = _node1;
-                node2 = _node2;
-            }
+			if (_node1.idOnSpline < _node2.idOnSpline || _node1.isEndPoint || (!isSameSpline && !_node2.isEndPoint))
+			{
+				node1 = _node1;
+				node2 = _node2;
+			}
+			else
+			{
+				node1 = _node2;
+				node2 = _node1;
+			}
+
+			if (node1.isEndPoint || node2.isEndPoint)
+			{
+				intersectionType = IntersectionTypeEnum.ThreeWay;
+			}
+			else
+			{
+				intersectionType = IntersectionTypeEnum.FourWay;
+			}
 
             node1.intersectionOtherNode = node2;
             node2.intersectionOtherNode = node1;
